6f596fda953eb337a5622c2f244959ab30efc523
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-10-08 02:29:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+let input = document.getElementById('input');
+
+function addingEventListener(event) {
+  input.addEventListener('click', function addingEventListener(event) {
+  alert('I was clicked!');
+});
 }
+
+
