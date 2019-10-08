452a9c80d767af7f32404eaf540f7181e3b7fdd6
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-10-08 02:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let input = document.getElementById('input');
 
 function addingEventListener(event) {
+<<<<<<< HEAD
   input.addEventListener('click', function addingEventListener(event) {
   alert('I was clicked!');
 });
 }
 
+=======
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', addingEventListener());
+>>>>>>> 69e69a274ca871e8dbe8d72eadc77aef700f9294
 
